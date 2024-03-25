dfa8804b52181b7772a5da0dd31f83242e706d3c
Thijs Moens <thijs@raketwetenschap.com>
2024-03-25 14:40:16 +0100

    Update

---
+++ Constants.py
@@ -0,0 +1,7 @@
+# Import libraries
+import os
+
+SEASONS = list(range(2016, 2023))
+DATA_DIR = "data"
+STANDINGS_DIR = os.path.join(DATA_DIR, "standings")
+SCORES_DIR = os.path.join(DATA_DIR, "scores")
